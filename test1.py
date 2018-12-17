fddb1b861d3d4c723adb2e23db58ab2dc075e29a
bartlomiejlu <45937461+bartlomiejlu@users.noreply.github.com>
2018-12-17 19:24:37 +0100

    Add files via upload
    Just a simple script in Python, revision 1.0

---
+++ test1.py
@@ -0,0 +1,32 @@
+import json
+import difflib
+from difflib import SequenceMatcher
+from difflib import get_close_matches
+
+data = json.load(open("data.json", "r"))
+def translate(word):
+    if word in data:
+        return data[word]
+        word = word.lower()
+    elif word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or NO if no" % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it"
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+output = translate(word)
+
+if type(output) == list:
+  for item in output:
+    print(item)
+else:
+    print(output)
+print(word)
